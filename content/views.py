eca562061fd9db9250bad15dd7fe2882b38bc937
fxxcxx <rkdrornf99@naver.com>
2022-06-16 15:16:55 +0900

    Feat : models.py and sqlite3

--- content/views.py
+++ content/views.py
@@ -5,11 +5,9 @@ from .models import Feed
 
 
 # Create your views here.
-
-
 class Main(APIView):
     def get(self, request):
-        feed_list = Feed.objects.all()  # feed에 있는 모든 데이터를 가져오겠다(select * from content_feed)
+        feed_list = Feed.objects.all().order_by('-id')  # feed에 있는 모든 데이터를 가져오겠다(select * from content_feed)
 
         for feed in feed_list:
             print(feed.content)
